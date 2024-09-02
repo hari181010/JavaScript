0a455901845032dba6d1cc38112f746c29f7748b
hari181010 <142649450+hari181010@users.noreply.github.com>
2024-09-02 16:12:10 +0000

    contsainer config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+let accountId=217
+var age=18
+const num=22
+//num=48 // not allowed
+console.log(num);
+
+
+accId=118
+accEmail="hari@gmail.com"
+accnum=222
+console.table([accId,accEmail,accnum]);
